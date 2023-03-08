27bc053b4febe64f16139ae2440a9890fa0efd3e
LYChubby <f4uzi345@gmail.com>
2023-03-08 16:01:59 +0700

    first commit

---
+++ Coba.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Selamat Datang";
+
+    return 0;
+}
---
+++ Latihan1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+  double Luas, P, L;
+  cout << "Masukkan Panjangnya : "; // input
+  cin >> P; // acc
+  cout << "Masukkan Lebar : ";
+  cin >> L;
+
+  Luas = P*L; // proses
+  cout << "Luasnya : " << Luas; // output
+}
